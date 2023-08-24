234688d3aff0d2fc7b8641c931aa94399416df9f
JamesJang-2jz <jhjx92@gmail.com>
2023-08-23 22:34:48 -0400

    created map to rows

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+      // console.log("james was here")
+      loadUsers();
+    }, [] );
+
+    const loadUsers = async () => {
+      const result = await axios.get("http://localhost:8080/crud/users")
+      setUsers(result.data);
+    }
+    
+
   return (
     <div className='container'>
       <div className='py-4'>
       <table className="table border shadow">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">result</th>
+      <th scope="col">Name</th>
+      <th scope="col">Username</th>
+      <th scope="col">@Email</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+
+     {
+        users.map((user, index) => (
+          <tr>
+            <th scope="row" key={user}>{index+1}</th>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+            
+          </tr>
+        ))
+      }
   </tbody>
+      
+
 </table>
 
       </div>
