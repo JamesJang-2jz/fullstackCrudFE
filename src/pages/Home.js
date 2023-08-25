a2ee5ee153a965f8b33c23773a2f9f476acb26cf
JamesJang-2jz <jhjx92@gmail.com>
2023-08-25 16:34:38 -0400

    updated onchange addUser

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,44 +14,40 @@ export default function Home() {
       const result = await axios.get("http://localhost:8080/crud/users")
       setUsers(result.data);
     }
-    
 
   return (
-    <div className='container'>
-      <div className='py-4'>
-      <table className="table border shadow">
-  <thead>
-    <tr>
-      <th scope="col">result</th>
-      <th scope="col">Name</th>
-      <th scope="col">Username</th>
-      <th scope="col">@Email</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
+  <div className='container'>
+    <div className='py-4'>
+    <table className="table border shadow">
+      <thead>
+        <tr>
+         <th scope="col">result</th>
+         <th scope="col">Name</th>
+         <th scope="col">Username</th>
+         <th scope="col">@Email</th>
+         <th scope="col">Action</th>
+       </tr>
+      </thead>
+        <tbody>
+          {
+              users.map((user, index) => (
+                <tr>
+                  <th scope="row" key={user}>{index+1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <button className='btn btn-primary mx-2 my-2'>View</button>
+                    <button className='btn btn-outline-primary mx-2 my-2'>Edit</button>
+                    <button className='btn btn-danger mx-2 my-2'>Delete</button>
+                  </td>
+                </tr>
+              ))
+            }
+        </tbody>
+      </table>
 
-     {
-        users.map((user, index) => (
-          <tr>
-            <th scope="row" key={user}>{index+1}</th>
-            <td>{user.name}</td>
-            <td>{user.username}</td>
-            <td>{user.email}</td>
-            <td>
-              <button className='btn btn-primary mx-2 my-2'>View</button>
-              <button className='btn btn-outline-primary mx-2 my-2'>Edit</button>
-              <button className='btn btn-danger mx-2 my-2'>Delete</button>
-            </td>
-          </tr>
-        ))
-      }
-  </tbody>
-      
-
-</table>
-
-      </div>
     </div>
+  </div>
   )
 }
