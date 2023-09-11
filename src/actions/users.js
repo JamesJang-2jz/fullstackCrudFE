5352dccd90ac1b290d3d31ef074c34986c4c7fb6
JamesJang-2jz <jhjx92@gmail.com>
2023-09-11 16:11:20 -0400

    create user action

--- src/actions/users.js
+++ src/actions/users.js
@@ -2,3 +2,18 @@ import { CREATE_USER, RETRIEVE_USERS, UPDATE_USER, DELETE_ALL_USERS, DELETE_USER
 
 import userdataService from "../services/userdata.service";
 
+export const createUser = (name, username, email) => async (dispatch) => {
+    try {
+        const result = await userdataService.create({name, username, email});
+
+        dispatch({
+            type: CREATE_USER,
+            payload: result.data
+        });
+
+        return Promise.resolve(result.data);
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
