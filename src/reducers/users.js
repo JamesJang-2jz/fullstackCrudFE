dc8138048522bf02159cd0633a906e49f802aac4
JamesJang-2jz <jhjx92@gmail.com>
2023-09-15 02:15:17 -0400

    added store.js file for redux but createStore deprecated

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { CREATE_USER, RETRIEVE_USERS, UPDATE_USER, DELETE_ALL_USERS, DELETE_USER } from "./types";
+import { CREATE_USER, RETRIEVE_USERS, UPDATE_USER, DELETE_ALL_USERS, DELETE_USER } from "../actions/types";
 
 const initialState = [];
 
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware } from "redux"; // use configureStore, createStore is deprecated
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from "redux-devtools-extension"; // named imports use curly braces
+import thunk from 'redux-thunk'; // default imports dont use curly braces
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = configureStore( // createStore is deprecated
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
