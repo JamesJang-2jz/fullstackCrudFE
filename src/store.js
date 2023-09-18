6e2a53f35ca9c63f3a7a561c62413738805c63b1
JamesJang-2jz <jhjx92@gmail.com>
2023-09-18 12:58:53 -0400

    comment rootReducer, reading more about redux toolkit

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { applyMiddleware } from "redux"; // use configureStore, createStore is d
 import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from "redux-devtools-extension"; // named imports use curly braces
 import thunk from 'redux-thunk'; // default imports dont use curly braces
-import rootReducer from './reducers';
+import rootReducer from './reducers'; // common redux pattern is to import from ./reducers and redux looks for default index.js file
 
 const initialState = {};
 
