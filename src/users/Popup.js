4b92172955bd76def13217431e6c82d0a668ec43
JamesJang-2jz <jhjx92@gmail.com>
2023-10-03 16:53:21 -0400

    adding modal component

---
+++ src/users/Popup.js
@@ -0,0 +1,11 @@
+// import { modal } from "bootstrap"
+
+export default function Popup ({handleDeleteTrue }) {
+    return (
+        <div className="modal">
+            <div className="modal_box">
+                <p></p>
+            </div>
+        </div>
+    )
+}
